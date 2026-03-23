1a2859807ee9b5f33d5701adddf840310d3cdcd5
zimoziny <zimoziny@duksung.ac.kr>
2026-03-24 03:32:02 +0900

    1장 과제 완료

---
+++ DP_2026-1/src/hw/ch01/Main.java
@@ -0,0 +1,29 @@
+package ch01.Sample;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("학번: 20231114 이름: 이다은");
+        
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Around the World in 80 Days"));
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+
+        // 명시적으로 Iterator를 사용하는 방법 
+        Iterator<Book> it = bookShelf.iterator();
+        while (it.hasNext()) {
+            Book book = it.next();
+            System.out.println(book.getName());
+        }
+        System.out.println();
+
+        // 확장 for문을 사용하는 방법 
+        for (Book book: bookShelf) {
+            System.out.println(book.getName());
+        }
+        System.out.println();
+    }
+}
