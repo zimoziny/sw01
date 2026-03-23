5d5f6ad229aa2063d4332d194dc4d9f928052952
zimoziny <zimoziny@duksung.ac.kr>
2026-03-24 07:07:40 +0900

    1장 과제 완료

---
+++ DP_2026-1/src/practice/ch01/BookShelf.java
@@ -0,0 +1,32 @@
+package practice.ch01;
+
+//import java.util.Iterator;
+
+//public class BookShelf implements Iterable<Book> {
+public class BookShelf {
+    private Book[] books;
+    private int last = 0;
+
+    public BookShelf(int maxsize) {
+        this.books = new Book[maxsize];
+    }
+
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+
+    public void appendBook(Book book) {
+        this.books[last] = book;
+        last++;
+    }
+
+    public int getLength() {
+        return last;
+    }
+
+    // CTRL+/
+    // @Override
+    // public Iterator<Book> iterator() {
+    //     return new BookShelfIterator(this);
+    // }
+}
