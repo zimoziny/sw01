48f7114b99d118326a24f4c9886cbdf53a5b1384
zimoziny <zimoziny@duksung.ac.kr>
2026-03-31 10:17:00 +0900

    2장 과제완료

--- DP_2026-1/src/hw/ch02/Main.java
+++ DP_2026-1/src/hw/ch02/Main.java
@@ -1,7 +1,9 @@
 package hw.ch02;
-
+    
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("20231114 이다은");
         // Step 1: 한국 콘센트 생성
         KoreanOutlet outlet = new KoreanOutlet();
 
